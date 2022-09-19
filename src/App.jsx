ad8b7ed32331b2ccff2d9cfdc6b35badeb214930
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-09-19 13:56:05 +0200

    added login/logout/signin-pages without styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { Routes, Route } from 'react-router-dom'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import './assets/scss/App.scss'
+// pages
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import LogoutPage from './pages/LogoutPage'
+import SignupPage from './pages/SignupPage'
 
 function App() {
 
   return (
     <div className="App">
 
-      <HomePage />
+      <Routes>
+              {/* Guest routes */}
+              {/* <Route path="*" element={<NotFound />} /> */}
+              <Route path="/" element={<HomePage />} />
+              {/* <Route path="/forgot-password" element={<ForgotPasswordPage />} /> */}
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/logout" element={<LogoutPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+
+              {/* Protected routes */}
+              
+      </Routes>
 
     </div>
   )
