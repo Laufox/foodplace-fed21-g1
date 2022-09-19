68cf72e9575336ef3ce2a1a22dcd06d88bb44279
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-09-19 14:51:20 +0200

    Adds basic map on home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,42 @@
+import { Container } from 'react-bootstrap'
+import { useJsApiLoader, GoogleMap, Marker} from '@react-google-maps/api'
+
+const center = { lat: 55.6032746, lng: 13.0165715 }
+
 const HomePage = () => {
 
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: import.meta.env.VITE_MAPS_API_KEY
+    })
+
     return (
-        <h1>This is homepage</h1>
+        <Container>
+            <h1>This is homepage</h1>
+
+            <div className='maps-wrapper'>
+                {
+                    !isLoaded && (
+                        <p>Loading google maps...</p>
+                    )
+                }
+
+                {
+                    isLoaded && (
+                        <GoogleMap
+                            center={center}
+                            zoom={15}
+                            mapContainerStyle={{width: '100%', height: '100%'}}
+
+                        >
+
+                            <Marker visible position={center} onClick={() => {console.log('Hello from marker')}} />
+                            <Marker position={{lat: 55, lng: 13}} />
+
+                        </GoogleMap>
+                    )
+                }
+            </div>
+        </Container>
     )
 
 }
