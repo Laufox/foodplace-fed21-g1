26a31bd5e591c807b9e6962b16100134f0915575
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-09-19 15:12:26 +0200

    Adds a marker to map

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap'
-import { useJsApiLoader, GoogleMap, Marker} from '@react-google-maps/api'
+import { useJsApiLoader, GoogleMap, MarkerF} from '@react-google-maps/api'
+import { useState } from 'react'
 
 const center = { lat: 55.6032746, lng: 13.0165715 }
 
@@ -10,7 +11,7 @@ const HomePage = () => {
     })
 
     return (
-        <Container>
+        <>
             <h1>This is homepage</h1>
 
             <div className='maps-wrapper'>
@@ -25,18 +26,17 @@ const HomePage = () => {
                         <GoogleMap
                             center={center}
                             zoom={15}
-                            mapContainerStyle={{width: '100%', height: '100%'}}
-
+                            mapContainerStyle={{width: '100%', height: '100%'}} options={{
+                                streetViewControl: false,
+                                mapTypeControl: false
+                            }}
                         >
-
-                            <Marker visible position={center} onClick={() => {console.log('Hello from marker')}} />
-                            <Marker position={{lat: 55, lng: 13}} />
-
+                            <MarkerF position={center} onClick={() => {console.log('Hello from marker')}} />
                         </GoogleMap>
                     )
                 }
             </div>
-        </Container>
+        </>
     )
 
 }
