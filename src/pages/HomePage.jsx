39ffb1a0e67837142e8ecf8ca3f4f552f23ec25f
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-09-21 15:23:41 +0200

    Commenting and restructuring

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,12 @@ import { useJsApiLoader, GoogleMap, MarkerF} from '@react-google-maps/api'
 import { useState, useEffect } from 'react'
 import SearchAddressForm from '../components/SearchAddressForm'
 import MapsAPI from '../services/mapsAPI'
+import userMarkerImg from '../assets/images/usermarker.png'
 
+// Array of library for maps api to include
+const libraries = ['places']
+
+// Hardcooded coordinates for food places, to be replaced by actual places from firebase
 const places = [
     {
         lat: 55.58,
@@ -22,28 +27,49 @@ const places = [
     },
 ]
 
+/**
+ *
+ * Home/default component to show interactive map of all food places
+ *
+ */
 const HomePage = () => {
 
+    // Initiate maps api script, giving it api key & libraries to include
     const { isLoaded } = useJsApiLoader({
         googleMapsApiKey: import.meta.env.VITE_MAPS_API_KEY,
-        libraries: ['places']
+        libraries
     })
 
+    // State for interactable map instance
     const [map, setMap] = useState(/** @type google.maps.Map */ (null))
+    // State for users current position
     const [userPosition, setUserPosition] = useState({lat: 55.6032746, lng: 13.0165715})
 
+    /**
+     *
+     * Function to handle when search form has been submitted
+     *
+     */
     const handleOnSubmit = async (address) => {
 
+        // If no address has been given, return
         if(!address) {
             return
         }
 
-        map.panTo(await MapsAPI.getLatAndLng(address))
+        // Get coordinates for address
+        const newCoords = await MapsAPI.getLatAndLng(address)
+        // Center map on the new coordinates
+        map.panTo(newCoords)
+
+        // Set current user position to new coords
+        setUserPosition(newCoords)
 
     }
 
     useEffect(() => {
 
+        // If user browser shares its geolocation, set current user position to those coordinates
         if ('geolocation' in navigator) {
             navigator.geolocation.getCurrentPosition((position) => {
                 setUserPosition({lat: position.coords.latitude, lng: position.coords.longitude})
@@ -67,8 +93,10 @@ const HomePage = () => {
                     isLoaded && (
 
                         <>
+                        {/* Form for centering map at different address */}
                         <SearchAddressForm onSubmit={handleOnSubmit} />
 
+                        {/* The map itself */}
                         <GoogleMap
                             center={userPosition}
                             zoom={15}
@@ -84,6 +112,9 @@ const HomePage = () => {
                             }}
                             onLoad={map => setMap(map)}
                         >
+                            {/* Marker for current user position */}
+                            <MarkerF position={userPosition} icon={userMarkerImg} onClick={() => {map.panTo(userPosition)}} />
+                            {/* Marker for each food place */}
                             {
                                 places.map( (place, index) => (
                                     <MarkerF key={index} position={place} onClick={() => {map.panTo(place)}} />
