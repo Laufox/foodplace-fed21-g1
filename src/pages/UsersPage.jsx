028a1259a81c7726a1f6ad295993b36bacd8004b
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-09-26 20:17:46 +0200

    created users page and tested switching between general users and admins

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import { getAuth } from 'firebase/auth'
+import { db } from '../firebase'
+// context
+import { useAuthContext } from '../contexts/AuthContext'
+// hooks
+import useUser from '../hooks/useUser'
 
 const UsersPage = () => {
+  const { currentUser } = useAuthContext()
+  const id = currentUser.uid
+  
+  console.log('currentuser id', id)
+  const { data, loading } = useUser(id)
+  
+  const isAdmin = data.admin
+
+  console.log('data', data)
+  console.log('admin', isAdmin)
   return (
-    <div>UsersPage</div>
+    <>
+      <div>UsersPage</div>
+      {/* test switching between general users and admins */}
+      {isAdmin ? <p>admin</p> : <p>general user</p>}
+
+    </>
   )
 }
 
