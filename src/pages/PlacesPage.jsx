566ccbaa37fe78c00679469533febed8c7ef6b7c
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-10-03 09:43:15 +0200

    Replaces admin places list to be table a sortable table instead

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -12,24 +12,73 @@ import FoodPlacesList from '../components/FoodPlacesList'
 import BeatLoader from 'react-spinners/BeatLoader'
 // bootstrap
 import Container from 'react-bootstrap/Container'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { useMemo } from 'react'
+import FoodPlacesTable from '../components/FoodPlacesTable'
 
 const PlacesPage = () => {
   const { data: foodPlaces, loading } = useGetPlaces()
   const { currentUser } = useAuthContext()
   const id = currentUser.uid
-  const { isAdmin } = useAdmin(id) 
+  const { isAdmin } = useAdmin(id)
+
+  const columns = useMemo( () => {
+
+    return [
+        {
+            Header: 'Name',
+            accessor: 'name'
+        },
+        {
+            Header: 'Address',
+            accessor: 'adress'
+        },
+        {
+            Header: 'Town',
+            accessor: 'town'
+        },
+        {
+            Header: 'Supply',
+            accessor: 'supply'
+        },
+        {
+            Header: 'Type',
+            accessor: 'type'
+        },
+        {
+            Header: 'Cuisine',
+            accessor: 'cuisine'
+        },
+        {
+            Header: 'Actions',
+            Cell: ({ row: { original: foodplace } }) => (
+                <Button
+                  variant="primary"
+                  size="sm"
+                  as={Link}
+                  to={`/places/${foodplace.id}`}
+                >
+                  Goto
+                </Button>
+            )
+        },
+    ]
+
+}, [] )
 
   return (
     <Container className="py-3">
         <h1 className="h-text-color-dark">Restaurants</h1>
         {loading && <BeatLoader  color='#F27166' /> }
-        {isAdmin 
-          ? <FoodPlacesList foodPlaces={foodPlaces}  />
+        {isAdmin
+          ? <FoodPlacesTable foodPlaces={foodPlaces} columns={columns} />
+          // ? <FoodPlacesList foodPlaces={foodPlaces}  />
           : <p>You do not have permission to edit. Please contact the administrator.</p>
         }
-        
+
     </Container>
-    
+
   )
 }
 
