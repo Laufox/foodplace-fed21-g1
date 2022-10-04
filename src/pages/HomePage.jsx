124fe7790aef711aff5729cf41e16417c5dc9e8c
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-10-04 13:32:58 +0200

    positioned infobox more to left to better fit smaller screens

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -130,7 +130,7 @@ const HomePage = () => {
                         {/* The map itself */}
                         <GoogleMap
                             center={userPosition}
-                            zoom={14}
+                            zoom={13}
                             mapContainerStyle={{width: '100%', height: '100%'}}
                             options={{
                                 streetViewControl: false,
@@ -149,6 +149,7 @@ const HomePage = () => {
                                 position={userPosition}
                                 icon={userMarkerImg}
                                 onClick={handleUserMarkerClick}
+                                zIndex={1000}
                             />
 
                             {/* Marker for each food place */}
@@ -164,11 +165,14 @@ const HomePage = () => {
                                 ) )
                             }
 
-                            {/* Info box component to show when user clicks food place marker on map */}
+                            {/* Info box component to show when user clicks food place marker on map*/}
                             {
                                 currentSelectedFoodPlace && (
                                     <InfoBox
-                                        position={currentSelectedFoodPlace.coords}
+                                        position={{
+                                            lat: currentSelectedFoodPlace.coords.lat,
+                                            lng: currentSelectedFoodPlace.coords.lng - 0.02
+                                        }}
                                         options={{
                                             closeBoxURL: '',
                                             enableEventPropagation: true
